b77d9d328eaf0fec5d72c94a219b706c995dc87d
Taekjin Jang <xorwls1500@naver.com>
2023-08-30 05:15:26 +0900

    feat : IntersectionObserver 활용하여 인피니티스크롤 구현

--- src/containers/IssueContainer.tsx
+++ src/containers/IssueContainer.tsx
@@ -1,25 +1,56 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import * as S from '../styles/Issue.styled';
 import {useRecoilValueLoadable} from 'recoil';
 import {issueListSelector} from '../recoil/issueState';
 import IssueItem from '../components/IssueItem';
 import {issueType} from '../types/IssueTypes';
+import useInfiniteScroll from '../hooks/useInfiniteScroll';
 
 const IssueContainer = () => {
-    const issueListLoadable = useRecoilValueLoadable<issueType[]>(issueListSelector(1));
+    const target = useRef<HTMLDivElement>(null);
     const [issues, setIssues] = useState<issueType[]>([]);
+
+    const {count} = useInfiniteScroll({
+        target: target,
+        targetArray: issues,
+        threshold: 0.2,
+        endPoint: 3,
+    });
+    const issueListLoadable = useRecoilValueLoadable<issueType[]>(issueListSelector(count));
 
     useEffect(() => {
         if (issueListLoadable.state === 'hasValue') {
             setIssues(issues => [...issues, ...issueListLoadable.contents]);
         }
     }, [issueListLoadable.contents]);
+    console.info(issues);
 
     switch (issueListLoadable.state) {
         case 'hasValue':
             return (
                 <>
-                    <S.IssueContainer>
+                    <S.IssueContainer ref={target}>
+                        {issues.map((issue, index) => {
+                            const item = <IssueItem key={issue.number} issue={issue} />;
+                            if ((index + 1) % 5 === 0) {
+                                const adItem = (
+                                    <S.AdImage
+                                        key={index}
+                                        src='/assets/img/ad_image.png'
+                                        alt='ad-image'
+                                    ></S.AdImage>
+                                );
+                                return [item, adItem];
+                            }
+                            return item;
+                        })}
+                    </S.IssueContainer>
+                </>
+            );
+        case 'loading':
+            return (
+                <>
+                    <S.IssueContainer ref={target}>
                         {issues.map((issue, index) => {
                             const item = <IssueItem key={issue.number} issue={issue} />;
                             if ((index + 1) % 5 === 0) {
@@ -36,8 +67,6 @@ const IssueContainer = () => {
                     </S.IssueContainer>
                 </>
             );
-        case 'loading':
-            return <div>s</div>;
         case 'hasError':
             return <div> error</div>;
     }
